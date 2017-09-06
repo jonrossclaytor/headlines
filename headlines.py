863c126e84264f8c0d1086c85a16dc156724ef44
Ross Claytor <johnrossclaytor@gmail.com>
2017-09-05 20:15:57 -0500

    using GET

--- headlines.py
+++ headlines.py
@@ -14,7 +14,7 @@ RSS_FEEDS = {'bbc':'https://feeds.bbci.co.uk/news/rss.xml'
              ,'cnn':'http://rss.cnn.com/rss/edition.rss'
              ,'fox':'http://feeds.foxnews.com/foxnews/latest'}
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def get_news():
     query = request.args.get('publication')
     if not query or query.lower() not in RSS_FEEDS:
@@ -25,7 +25,7 @@ def get_news():
     feed = feedparser.parse(RSS_FEEDS[publication])
     #first_article = feed['entries'][0]
     
-    return render_template('home.html', articles = feed['entries'], publication=publication)
+    return render_template('home.html', articles = feed['entries'])
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
